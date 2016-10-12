6586eeae1e514d77f495559fb7548f0f340f0bfc
Brian Lin <brianlin224@gmail.com>
2016-10-12 19:48:22 +0800

    add animation

--- Scripts/js/common.js
+++ Scripts/js/common.js
@@ -4,20 +4,41 @@
 	var common = new page();
 
 	function page() {
-		this._menu = '.jq-menu';
+		this._menu      = '.jq-menu';
+		this._share     = '.jq-share';
+		this._slideDown = '.jq-slide-down';
 	}
 
-	page.prototype.showFooter = function() {
+	// 點擊目標區域以外的地方可關閉目標區域
+	page.prototype.offClick = function() {
+		projects.$d.off('click').on('click' , function(e){
+			e.stopPropagation();
+
+			if (!$(e.target).is('.m-nav-menu, .m-nav-menu *, ' + common._menu + ', ' + common._menu + ' *')) {
+				$(common._menu).removeClass('is-hover');
+			}
+		});
 	}
 
 	projects.$w.load(function(){
+		projects.FBInit();
+		common.offClick();
+
 		$(common._menu).on('click', function(){
 			$(this).toggleClass('is-hover');
+		});
+
+		$(common._share).on('click', function(){
+			projects.FBShare();
+		});
+
+		$(common._slideDown).on('click', function(){
+			projects.$hb.animate({'scrollTop': $(this).parent('.section-cut').next().offset().top}, 400);
 		});
 	});
 
 	projects.$d.ready(function(){
-		if ( projects.device() === 'Mobile') {
+		if (projects.device() === 'Mobile') {
 			projects.owlCarousel('.jQ-owl-xs');
 		}
 	});
--- Scripts/js/factory.js
+++ Scripts/js/factory.js
@@ -843,6 +843,31 @@
         } , (speed ? speed : 500));
     };
 
+    factory.prototype.FBInit = function() {
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId   : '1783545831931137',
+                xfbml   : true,
+                version : 'v2.8'
+            });
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    };
+
+    factory.prototype.FBShare = function() {
+        FB.ui({
+            method : 'share',
+            href   : 'http://richart-friend.sugarfun.com.tw/',
+        } , function(response) {});
+    };
+
     if ( ! window.projects ) {
         window.projects = projects;
     }
